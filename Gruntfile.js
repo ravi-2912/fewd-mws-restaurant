9d99f1413595137bb5b64077ab3c82016a752379
Ravinder Singh <ravi_29_12@hotmail.com>
2018-07-05 19:25:27 +0100

    Gruntfile for creating responsive minified images.

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        responsive_images: {
+            task_1: {
+                options: {
+                    /*engine: 'im',*/
+                    sizes: [{
+                        name: "1x",
+                        width: 400,
+                        quality: 90
+                    }, {
+                        name: "2x",
+                        width: 800,
+                        quality: 90
+                    }]
+                },
+
+                files: [{
+                    expand: true,
+                    src: ['*.{gif,jpg,png}'],
+                    cwd: 'img_src',
+                    dest: 'img'
+                }]
+            },
+        },
+
+        /* Clear out the images directory if it exists */
+        clean: {
+            dev: {
+                src: ['img/']
+            },
+        },
+
+        /* Generate the images directory if it is missing */
+        mkdir: {
+            dev: {
+                options: {
+                    create: ['img']
+                },
+            },
+        },
+    });
+  
+    grunt.loadNpmTasks('grunt-responsive-images');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-mkdir');
+    grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images']);
+
+};
+
+/*
+npm install grunt-mkdir --save-dev
+npm install grunt-contrib-clean --save-dev
+npm install grunt-contrib-copy --save-dev
+npm install grunt-responsive-images --save-dev*/
